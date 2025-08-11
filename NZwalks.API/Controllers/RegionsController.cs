8e9a65389f6921e46357c0e4015335e392fbf781
Mahdi8316 <mahdi.zeinali8316@gmail.com>
2025-08-11 19:09:14 +0430

    implement get action method without using DTO

---
+++ NZwalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZwalks.API.Data;
+
+namespace NZwalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+
+            //var region = dbContext.Regions.Find(id); just for primary key
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+    }
+}
